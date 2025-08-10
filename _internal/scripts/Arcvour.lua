02f264f52f1ae233e70711413b4ae2228d59030c
Fami Andara Haqi <127956909+Fami-dev@users.noreply.github.com>
2025-08-10 09:07:54 +0700

    Create Arcvour.lua

---
+++ _internal/scripts/Arcvour.lua
@@ -0,0 +1,15 @@
+local gameScripts = {
+    [123921593837160] = "https://look-pearl.vercel.app/scripts/CAJTPMV3.lua",
+    [121864768012064] = "https://look-pearl.vercel.app/scripts/FI.lua",
+    [89343390950953] = "https://look-pearl.vercel.app/scripts/MSB.lua",
+    [110931811137535] = "https://look-pearl.vercel.app/scripts/CAFAB.lua",
+    [129827112113663] = "https://look-pearl.vercel.app/scripts/PROSPEC.lua"
+}
+
+local scriptUrl = gameScripts[game.PlaceId]
+
+if scriptUrl then
+    pcall(function()
+        loadstring(game:HttpGet(scriptUrl))()
+    end)
+end
